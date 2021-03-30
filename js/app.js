12e7e9082dacfbd8bb548970050ca50e8187e9ea
Sufmik <sufmik@gmail.com>
2021-03-30 16:47:32 +0300

    0.2 неверный вариант

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 setInterval(myFuncSuper, 1000);
     function myFuncSuper() {
+        let clean = document.querySelector("time")
+        if (clean.parentNode) {
+            clean.parentNode.removeChild(clean)
+        }
+        console.log(clean)
+
         let timeNow = Date()
-            document.write(timeNow)
+        document.write(timeNow)
     }
