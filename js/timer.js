eb2dbd2c05523afbdbc06d2ab65ed99a5530f503
Sufmik <74490460+Sufmik@users.noreply.github.com>
2021-04-04 11:08:56 +0300

    1.3

--- js/timer.js
+++ js/timer.js
@@ -3,6 +3,7 @@ setInterval(myFuncSuper, 1000);
 function myFuncSuper() {
     document.querySelector(".time").innerHTML = ""
 
+
 let timeNow = new Date().toLocaleTimeString()
     document.querySelector(".time").innerHTML = (timeNow)
 }
