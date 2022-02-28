347e221b96aa4b0134b84a32763001b3c981b749
Sufmik <74490460+Sufmik@users.noreply.github.com>
2022-02-28 23:25:34 +0300

    1.8.5

--- js/compare.js
+++ js/compare.js
@@ -30,6 +30,20 @@ const compare = () => {
                 city.push(opt.value)
                 localStorage.setItem('city', JSON.stringify(city))
                 console.log(city);
+
+                /*let result = (value) => {
+                    let city = (JSON.parse(localStorage.getItem('city')))
+
+                    let newCity = city.map(item => {
+                        if (item.value === value) {
+                            return item
+                        }
+                    })
+                    localStorage.setItem('city', JSON.stringify(newCity))
+                }
+                result()
+                console.log(result());*/
+
             } else if (city.length > 0) {
                 city.pop()
                 city.push(opt.value)
