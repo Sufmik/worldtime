7d9fe15331736f7baa29b48318b6bea9ab0b50bd
Sufmik <74490460+Sufmik@users.noreply.github.com>
2022-03-17 22:52:06 +0300

    2.0

--- js/timer_new.js
+++ js/timer_new.js
@@ -1,10 +1,58 @@
 const timer = () => {
     let timeNow = new Date()
 
-    data = {
-        time: 1,
-        src: '<img src=nums/num/0.svg>',
-    }
+    const data = [
+
+        {
+            time: 0,
+            src: '<img src=nums/num/0.svg>',
+        },
+
+        {
+            time: 1,
+            src: '<img src=nums/num/1.svg>',
+        },
+
+        {
+            time: 2,
+            src: '<img src=nums/num/2.svg>',
+        },
+
+        {
+            time: 3,
+            src: '<img src=nums/num/3.svg>',
+        },
+
+        {
+            time: 4,
+            src: '<img src=nums/num/4.svg>',
+        },
+
+        {
+            time: 5,
+            src: '<img src=nums/num/5.svg>',
+        },
+
+        {
+            time: 6,
+            src: '<img src=nums/num/6.svg>',
+        },
+
+        {
+            time: 7,
+            src: '<img src=nums/num/7.svg>',
+        },
+
+        {
+            time: 8,
+            src: '<img src=nums/num/8.svg>',
+        },
+
+        {
+            time: 9,
+            src: '<img src=nums/num/9.svg>',
+        },
+    ]
 
     //час
     const h = timeNow.getHours()
@@ -23,9 +71,49 @@ const timer = () => {
 
     //определение нужного числа
 
-    if (h1 === data.time) {
-        document.querySelector('.hour').innerHTML = data.src
-    }
+    data.find(value => {
+        if (h1 == value.time) {
+            const index = data.find(value => value.time == h1).src
+            document.querySelector('.hour').innerHTML = (index)
+        }
+    })
+
+    data.find(value => {
+        if (h2 == value.time) {
+            const index = data.find(value => value.time == h2).src
+            document.querySelector('.hour1').innerHTML = (index)
+        }
+    })
+
+    data.find(value => {
+        if (h1 == value.time) {
+            const index = data.find(value => value.time == m1).src
+            document.querySelector('.minute').innerHTML = (index)
+        }
+    })
+
+    data.find(value => {
+        if (h1 == value.time) {
+            const index = data.find(value => value.time == m2).src
+            document.querySelector('.minute1').innerHTML = (index)
+        }
+    })
+
+    data.find(value => {
+        if (h1 == value.time) {
+            const index = data.find(value => value.time == s1).src
+            document.querySelector('.seconds').innerHTML = (index)
+        }
+    })
+
+    data.find(value => {
+        if (h1 == value.time) {
+            const index = data.find(value => value.time == s2).src
+            document.querySelector('.seconds1').innerHTML = (index)
+        }
+    })
 }
 
 timer()
+
+setInterval(timer, 1000)
